b673dc58064df8082794e0f7df78e9085d2e3a76
Michel <michel.capitanio@evogy.it>
2020-03-19 10:05:37 +0100

    updated dependecies an python runtime

--- util/push.py
+++ util/push.py
@@ -5,8 +5,7 @@ from models.push import Push
 @celery.task(bind=True)
 def send_push(self, message, installations, push_id):
     total = len(installations)
-    i = 0
-    for installation in installations:
+    for key, installation in enumerate(installations):
         response = sns_client.publish(
             TargetArn=installation['endpoint_arn'],
             Message=message
@@ -15,9 +14,8 @@ def send_push(self, message, installations, push_id):
             'device_id':installation['device_id'],
             'message_id':response['MessageId'],
             'total':total,
-            'current':i
+            'current':key
         })
-        i += 1
     push = Push.query.filter_by(
         uuid=push_id
     ).one_or_none()
